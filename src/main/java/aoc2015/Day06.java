4460658d075c559feac432f5ff1e03c113548175
H-Rusch <kirdneh98mail@gmail.com>
2023-03-28 19:53:51 +0200

    2015 day 6

---
+++ src/main/java/aoc2015/Day06.java
@@ -0,0 +1,95 @@
+package aoc2015;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+class Day06 extends Day {
+
+    private final int size = 1000;
+    // for part 1
+    private boolean[][] grid = new boolean[size][size];
+    // for part 2
+    private int[][] lights = new int[size][size];
+
+    private final Pattern pattern = Pattern.compile("^(turn on|turn off|toggle) (\\d+),(\\d+) through (\\d+),(\\d+)$");
+
+    Day06() {
+        super(6);
+    }
+
+    @Override
+    public String part1(String input) {
+        input.lines().forEach(line -> executeCommand(line, true));
+
+        return String.valueOf(countLightsOn());
+    }
+
+    @Override
+    public String part2(String input) {
+        input.lines().forEach(line -> executeCommand(line, false));
+
+        return String.valueOf(calculateTotalLights());
+    }
+
+    private int countLightsOn() {
+        int count = 0;
+        for (boolean[] row : grid) {
+            for (boolean b : row) {
+                if (b) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    private int calculateTotalLights() {
+        return Arrays.stream(lights).mapToInt(row -> Arrays.stream(row).sum()).sum();
+    }
+
+    private void executeCommand(String line, boolean part1) {
+        Matcher matcher = pattern.matcher(line);
+
+        if (matcher.matches() && matcher.groupCount() == 5) {
+            int fromX = Integer.parseInt(matcher.group(2));
+            int fromY = Integer.parseInt(matcher.group(3));
+            int toX = Integer.parseInt(matcher.group(4));
+            int toY = Integer.parseInt(matcher.group(5));
+
+            if (part1) {
+                changeLights(matcher.group(1), fromX, toX, fromY, toY);
+            } else {
+                changeBrightness(matcher.group(1), fromX, toX, fromY, toY);
+            }
+        }
+    }
+
+    private void changeLights(String command, int fromX, int toX, int fromY, int toY) {
+        for (int y = fromY; y <= toY; y++) {
+            for (int x = fromX; x <= toX; x++) {
+                grid[y][x] = switch (command) {
+                    case "turn on" -> true;
+                    case "turn off" -> false;
+                    default -> !grid[y][x];
+                };
+            }
+        }
+    }
+
+    private void changeBrightness(String command, int fromX, int toX, int fromY, int toY) {
+        for (int y = fromY; y <= toY; y++) {
+            for (int x = fromX; x <= toX; x++) {
+                lights[y][x] = switch (command) {
+                    case "turn on" -> lights[y][x] + 1;
+                    case "turn off" -> Math.max(0, lights[y][x] - 1);
+                    default -> lights[y][x] + 2;
+                };
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new Day06().main();
+    }
+}
---
+++ src/test/java/aoc2015/Day06Test.java
@@ -0,0 +1,25 @@
+package aoc2015;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Day06Test {
+
+    @Test
+    void part1Test() {
+        var day = new Day06();
+
+        assertEquals("1000000", day.part1("turn on 0,0 through 999,999"));
+        assertEquals("999000", day.part1("toggle 0,0 through 999,0"));
+        assertEquals("998996", day.part1("turn off 499,499 through 500,500"));
+    }
+
+    @Test
+    void part2Test() {
+        var day = new Day06();
+
+        assertEquals("1", day.part2("turn on 0,0 through 0,0"));
+        assertEquals("2000001", day.part2("toggle 0,0 through 999,999"));
+    }
+}
