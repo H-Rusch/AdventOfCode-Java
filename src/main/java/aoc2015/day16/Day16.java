683bcd744cdf8cc574acdf26f40a1a3ea67a4fd8
Hendrik Rusch <hendrik.rusch@senacor.com>
2023-09-15 17:36:29 +0200

    2015 day 16

---
+++ src/main/java/aoc2015/day16/Day16.java
@@ -0,0 +1,77 @@
+package aoc2015.day16;
+
+import aoc2015.Day2015;
+
+import java.util.List;
+
+public class Day16 extends Day2015 {
+
+    private SueMatcher sueMatcher;
+
+    public Day16() {
+        super(16);
+    }
+
+    @Override
+    public Object part1(String input) {
+        var sues = parseInput(input);
+        sueMatcher = new SueMatcher(createExampleSue());
+
+        return findExactlyMatchingSue(sues);
+    }
+
+    @Override
+    public Object part2(String input) {
+        var sues = parseInput(input);
+        sueMatcher = new SueMatcher(createExampleSue());
+
+        return findMatchingSueUsingRanges(sues);
+    }
+
+    public Sue createExampleSue() {
+        Sue sue = new Sue();
+        var fields = sue.getFields();
+        fields.put("children", 3);
+        fields.put("cats", 7);
+        fields.put("samoyeds", 2);
+        fields.put("pomeranians", 3);
+        fields.put("akitas", 0);
+        fields.put("vizslas", 0);
+        fields.put("goldfish", 5);
+        fields.put("trees", 3);
+        fields.put("cars", 2);
+        fields.put("perfumes", 1);
+
+        return sue;
+    }
+
+    public int findExactlyMatchingSue(List<Sue> sues) {
+        for (int i = 0; i < sues.size(); i++) {
+            if (sueMatcher.matchesExactly(sues.get(i))) {
+                return i + 1; // numbering begins with 1
+            }
+        }
+
+        throw new NoMatchingSueFoundException("Not found");
+    }
+
+    public int findMatchingSueUsingRanges(List<Sue> sues) {
+        for (int i = 0; i < sues.size(); i++) {
+            if (sueMatcher.matchesWithRanges(sues.get(i))) {
+                return i + 1; // numbering begins with 1
+            }
+        }
+
+        throw new NoMatchingSueFoundException("Not found");
+    }
+
+    public static List<Sue> parseInput(String input) {
+        return input.lines()
+                .map(Sue::fromString)
+                .toList();
+    }
+
+    public static void main(String[] args) {
+        new Day16().main();
+    }
+}
---
+++ src/main/java/aoc2015/day16/NoMatchingSueFoundException.java
@@ -0,0 +1,7 @@
+package aoc2015.day16;
+
+public class NoMatchingSueFoundException extends RuntimeException {
+    public NoMatchingSueFoundException(String message) {
+        super(message);
+    }
+}
