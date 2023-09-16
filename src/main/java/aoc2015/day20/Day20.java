5f874b835aa017add61d1b399d2baf79cf8f2ad3
Hendrik Rusch <hendrik.rusch@senacor.com>
2023-09-16 20:01:25 +0200

    2015 day 15 part 2

--- src/main/java/aoc2015/day20/Day20.java
+++ src/main/java/aoc2015/day20/Day20.java
@@ -5,6 +5,10 @@ import aoc2015.Day2015;
 
 public class Day20 extends Day2015 {
 
+    private static final int ELF_PRESENTS_1 = 10;
+    private static final int ELF_PRESENTS_2 = 11;
+    private static final int ELF_REPEATS = 50;
+
     private int[] housePresents;
 
     public Day20() {
@@ -14,23 +18,41 @@ public class Day20 extends Day2015 {
     @Override
     public Object part1(String input) {
         var targetPresents = Integer.parseInt(input.trim());
-        fillPresentArray(targetPresents);
+        fillPresentArrayPart1(targetPresents);
 
         return findLowestHouseNumber(targetPresents);
     }
 
     @Override
     public Object part2(String input) {
-        return null;
+        var targetPresents = Integer.parseInt(input.trim());
+        fillPresentArrayPart2(targetPresents);
+
+        return findLowestHouseNumber(targetPresents);
     }
 
-
-    private void fillPresentArray(int targetPresents) {
-        this.housePresents = new int[targetPresents / 10];
+    private void fillPresentArrayPart1(int targetPresents) {
+        this.housePresents = new int[targetPresents / ELF_PRESENTS_1];
 
         for (int i = 1; i < housePresents.length; i++) {
             for (int j = i; j < housePresents.length; j += i) {
-                housePresents[j] += 10 * i;
+                housePresents[j] += ELF_PRESENTS_1 * i;
+            }
+        }
+    }
+
+    private void fillPresentArrayPart2(int targetPresents) {
+        this.housePresents = new int[targetPresents / ELF_PRESENTS_2];
+
+        for (int i = 1; i < housePresents.length; i++) {
+            int count = 0;
+            for (int j = i; j < housePresents.length; j += i) {
+                housePresents[j] += ELF_PRESENTS_2 * i;
+
+                count++;
+                if (count == ELF_REPEATS) {
+                    break;
+                }
             }
         }
     }
@@ -38,7 +60,7 @@ public class Day20 extends Day2015 {
     private int findLowestHouseNumber(int targetPresents) {
         for (int i = 0; i < housePresents.length; i++) {
             if (housePresents[i] >= targetPresents) {
-                return i + 1;
+                return i;
             }
         }
         return -1;
