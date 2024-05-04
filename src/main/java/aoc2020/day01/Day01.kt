6d3bdb85e62a1904bf2e7bd4c8fe9ed9ddb9489b
H-Rusch <kirdneh98mail@gmail.com>
2024-05-04 13:02:26 +0200

    2020 day 1

---
+++ src/main/java/aoc2020/day01/Day01.kt
@@ -0,0 +1,45 @@
+package aoc2020.day01
+
+import aoc2020.AbstractDay2020
+
+fun main() {
+    Day01().executeParts()
+}
+
+class Day01 : AbstractDay2020(1) {
+
+    private val target = 2020
+
+    override fun part1(input: String): Any {
+        val numbers: List<Int> = parseInput(input)
+
+        for (i in 0 until numbers.size - 1) {
+            for (j in i until numbers.size) {
+                if (numbers[i] + numbers[j] == target) {
+                    return numbers[i] * numbers[j]
+                }
+            }
+        }
+
+        return Unit
+    }
+
+    override fun part2(input: String): Any {
+        val numbers: List<Int> = parseInput(input)
+
+        for (i in 0 until numbers.size - 2) {
+            for (j in i until numbers.size - 1) {
+                for (k in j until numbers.size) {
+                    if (numbers[i] + numbers[j] + numbers[k] == target) {
+                        return numbers[i] * numbers[j] * numbers[k]
+                    }
+                }
+            }
+        }
+
+        return Unit
+    }
+
+    fun parseInput(input: String): List<Int> =
+        input.lines().stream().map { str -> str.toInt() }.toList()
+}
