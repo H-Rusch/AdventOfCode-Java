4441c10602b1a5d0b4863139b52ff7a511ceff1f
H-Rusch <kirdneh98mail@gmail.com>
2024-08-10 22:02:36 +0200

    2020 day 6

---
+++ src/test/java/aoc2020/day06/Day06Test.kt
@@ -0,0 +1,30 @@
+package aoc2020.day06
+
+import aoc.loadExample
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class Day06Test {
+
+    val input: String = loadExample(2020, "day06.txt")
+
+    private lateinit var subject: Day06
+
+    @BeforeEach
+    fun setUp() {
+        subject = Day06()
+    }
+
+    @Test
+    fun part1_example() {
+        assertThat(subject.part1(input))
+            .isEqualTo(11)
+    }
+
+    @Test
+    fun part2_example() {
+        assertThat(subject.part2(input))
+            .isEqualTo(6)
+    }
+}
